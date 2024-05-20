7bde12450d7c9c67bbd6e29fcfa11154e474d3a4
haeckal <haeckal@asaren.ai>
2024-05-20 15:40:20 +0700

    change user

--- app/src/db.js
+++ app/src/db.js
@@ -1,7 +1,7 @@
 const Pool = require('pg').Pool;
 
 const pool = new Pool({
-    user: "mhalkaaf",
+    user: "postgres",
     host: "database",
     database: "calories-api",
     password: "aTinsELrONveMONSITCHUcKeTCHeNT",
