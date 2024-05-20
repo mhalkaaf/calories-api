1f90a989c5196b2a638efd8082b0efb73b200001
haeckal <haeckal@asaren.ai>
2024-05-20 15:44:06 +0700

    fix queries

--- app/src/queries.js
+++ app/src/queries.js
@@ -1,9 +1,9 @@
 const getUser = "SELECT * FROM users";
 const getUserById = "SELECT * FROM users WHERE id = $1";
 const checkEmailExists = "SELECT s FROM users s WHERE s.email = $1";
-const addUsers = "INSERT INTO users (name, email, password) VALUES ($1, $2, $3)";
-const deleteUsers = "DELETE FROM students WHERE id = $1";
-const updateUsers = "UPDATE students SET name = $1 WHERE id = $2";
+const addUser = "INSERT INTO users (name, email, password) VALUES ($1, $2, $3)";
+const deleteUser = "DELETE FROM students WHERE id = $1";
+const updateUser = "UPDATE students SET name = $1 WHERE id = $2";
 
 module.exports = {
     getUser,
