5ba1458eae954483fcb1772536aa3dc125ec1f81
haeckal <haeckal@asaren.ai>
2024-05-20 17:53:32 +0700

    change database

--- app/src/queries.js
+++ app/src/queries.js
@@ -1,9 +1,9 @@
 const getUser = "SELECT * FROM users";
 const getUserById = "SELECT * FROM users WHERE id = $1";
 const checkEmailExists = "SELECT s FROM users s WHERE s.email = $1";
-const addUser = "INSERT INTO users (name, email, password) VALUES ($1, $2, $3)";
-const deleteUser = "DELETE FROM students WHERE id = $1";
-const updateUser = "UPDATE students SET name = $1 WHERE id = $2";
+const addUser = "INSERT INTO users (username, email, password) VALUES ($1, $2, $3)";
+const deleteUser = "DELETE FROM users WHERE id = $1";
+const updateUser = "UPDATE users SET username = $1 WHERE id = $2";
 
 module.exports = {
     getUser,
