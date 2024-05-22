cce3ab744e756b2388d6bccdd8c360c73a98baac
RaflyPutraSantoso380 <Rafly.putrasantoso28@.gmail.com>
2024-05-22 09:45:17 +0700

    buat branch baru

---
+++ app/src/signup.js
@@ -0,0 +1,30 @@
+const addUser = (req, res) => {
+
+    const { username, email, password } = req.body;
+
+    // check if emails is already exist
+    pool.query(queries.checkEmailExists, [email], (error, results) => {
+        if (error) {
+            console.error("Error executing query", error.stack);
+            return res.status(500).send("Internal server error");
+        }
+        if (results.rows.length) {
+            return res.status(400).send("Email already exists.");
+        }
+
+        // add student to database
+        pool.query(queries.addUser, [username, email, password], (error, results) => {
+            if (error) {
+                console.error("Error executing query", error.stack);
+                return res.status(500).send("Internal server error");
+            }
+            return res.status(200).send("Student added successfully");
+        });
+    });
+
+    
+};
+
+export{
+    addUser,
+};
