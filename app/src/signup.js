2433f587b288444dc54d67f6c7b1de9389746d59
RaflyPutraSantoso380 <Rafly.putrasantoso28@.gmail.com>
2024-05-22 11:18:19 +0700

    nambah hashing

--- app/src/signup.js
+++ app/src/signup.js
@@ -1,12 +1,13 @@
 import { pool } from './db.js';
 import * as queries from './queries.js';
+import bcrypt from 'bcrypt';
 
 const addUser = (req, res) => {
 
     const { username, email, password } = req.body;
 
     // check if emails is already exist
-    pool.query(queries.checkEmailExists, [email], (error, results) => {
+    pool.query(queries.checkEmailExists, [email], async (error, results) => { // Tambahkan async di sini
         if (error) {
             console.error("Error executing query", error.stack);
             return res.status(500).send("Internal server error");
@@ -15,18 +16,25 @@ const addUser = (req, res) => {
             return res.status(400).send("Email already exists.");
         }
 
-        // add student to database
-        pool.query(queries.addUser, [username, email, password], (error, results) => {
-            if (error) {
-                console.error("Error executing query", error.stack);
-                return res.status(500).send("Internal server error");
-            }
-            return res.status(200).send("Student added successfully");
-        });
-    });
+        try {
+            // Hash kata sandi
+            const hashedPassword = await bcrypt.hash(password, 10); // Menggunakan 10 putaran salt
 
-    
+            // Tambahkan pengguna ke database dengan kata sandi yang dihash
+            pool.query(queries.addUser, [username, email, hashedPassword], (error, results) => {
+                if (error) {
+                    console.error("Error executing query", error.stack);
+                    return res.status(500).send("Internal server error");
+                }
+                return res.status(200).send("Student added successfully");
+            });
+        } catch (hashError) {
+            console.error("Error hashing password", hashError.stack);
+            return res.status(500).send("Internal server error");
+        }
+    });
 };
+
 
 export{
     addUser,
