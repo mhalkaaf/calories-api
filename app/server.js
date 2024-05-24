bb9490dedad69bfff4efee040bbba86703e5867d
haeckal <haeckal@asaren.ai>
2024-05-24 22:44:11 +0700

    add body-parser

--- app/server.js
+++ app/server.js
@@ -3,6 +3,7 @@ import express from 'express';
 import cors from 'cors';
 import { router } from './src/routes/routes.js';
 import "dotenv/config.js";
+import bodyparser from 'body-parser';
 
 const app = express();
 const port = 3000;
@@ -10,6 +11,8 @@ const port = 3000;
 // middleware
 app.use(express.json());
 app.use(cors());
+app.use(bodyparser.urlencoded({ extended: false }));
+app.use(bodyparser.json());
 
 app.get('/', (req, res) => {
     res.send("Hello Folks!")
