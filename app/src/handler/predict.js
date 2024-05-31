410d398eeb5192ac9551ec653c933a7036e506e4
haeckal <haeckal@asaren.ai>
2024-05-31 21:11:01 +0700

    add logging

--- app/src/handler/predict.js
+++ app/src/handler/predict.js
@@ -26,10 +26,12 @@ const predict = async (req, res) => {
         const predictions = model.predict(inputTensor);
         // const probabilities = predictions.arraySync();
         const probabilities = predictions.arraySync()[0];
+        console.log(probabilities);
 
         // Determine the predicted fruit class
         const foodClasses = ['Apple', 'Banana', 'Fried_Chicken', 'Beef_Rendang', 'Egg', 'Doughnut'];
         const predictedClassIndex = probabilities.indexOf(Math.max(...probabilities));
+        console.log(predictedClassIndex);
         const predictedFood = foodClasses[predictedClassIndex];
 
         // return predictedFruit;
