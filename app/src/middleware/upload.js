9d4dc72e3f647126c37fbe249ddc276734c7337d
haeckal <haeckal@asaren.ai>
2024-05-31 16:06:11 +0700

    add dir

--- app/src/middleware/upload.js
+++ app/src/middleware/upload.js
@@ -1,5 +1,13 @@
 import multer from "multer";
 import path from "path";
+import fs from 'fs';
+
+const uploadDir = './uploads';
+
+// Check if the uploads directory exists, if not, create it
+if (!fs.existsSync(uploadDir)) {
+    fs.mkdirSync(uploadDir);
+}
 
 // Configure multer for file upload
 const storage = multer.diskStorage({
