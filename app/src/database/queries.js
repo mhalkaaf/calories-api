b94eb6d9322a724210dabdac0fb14ae01fce6d2c
haeckal <haeckal@asaren.ai>
2024-06-01 13:58:35 +0700

    update queries

--- app/src/database/queries.js
+++ app/src/database/queries.js
@@ -1,6 +1,5 @@
 const selectUser = "SELECT * FROM users where email = $1";
 const addUser = "INSERT INTO users (name, email, password) VALUES ($1, $2, $3) RETURNING *";
-// const insertCaloriesData = "INSERT INTO calorie_calculations (user_id, meal, calories) VALUES ${params.join(',')} RETURNING *";
 const insertCaloriesData = (mealsLength) => {
     const valuePlaceholders = [];
     for (let i = 0; i < mealsLength; i++) {
@@ -9,15 +8,6 @@ const insertCaloriesData = (mealsLength) => {
     }
     return `INSERT INTO calories (user_id, title, meals, calories) VALUES ${valuePlaceholders.join(', ')} RETURNING *`;
 };
-
-
-// const addCalorieCalculation = async (meals, calories) => {
-//     const result = await pool.query(
-//         'INSERT INTO calories (meal, calories) VALUES ($1, $2) RETURNING *',
-//         [meals, calories]
-//     );
-//     return result.rows[0];
-// };
 
 
 export {
