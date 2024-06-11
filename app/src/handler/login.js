b73a8ca5f9ab4b341b8e2839f1f8d6550686411d
haeckal <haeckal@asaren.ai>
2024-06-11 20:42:56 +0700

    apply

--- app/src/handler/login.js
+++ app/src/handler/login.js
@@ -30,7 +30,7 @@ const login = (validInfo, async (req, res) => {
 
         // 4. give them jwt token
 
-        const token = jwtGenerator(user.rows[0].id);
+        const token = jwtGenerator(user.rows[0].user_id);
         return res.json({ token });
         } catch (err) {
             console.error(err.message);
