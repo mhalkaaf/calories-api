533999a5f61d77a9c70ef27ef1523820eed7b5b3
haeckal <haeckal@asaren.ai>
2024-06-11 20:10:35 +0700

    adding id

--- app/src/database/queries.js
+++ app/src/database/queries.js
@@ -4,7 +4,7 @@ const addUser = "INSERT INTO users (name, email, password) VALUES ($1, $2, $3) R
 
 const addItem = "INSERT INTO calories (user_id, meal, amount) VALUES ($1, $2, $3) RETURNING *";
 
-const getItem = "SELECT meal, amount FROM calories WHERE user_id = $1";
+const getItem = "SELECT id, meal, amount FROM calories WHERE user_id = $1";
 
 const updateItem = "UPDATE calories SET meal = $1, amount = $2 WHERE user_id = $3 AND id = $4 RETURNING *;"
 
